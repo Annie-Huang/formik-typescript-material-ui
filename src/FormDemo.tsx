3a10130fe3547d9b5b8ed497715e17767cba173c
Annie Huang <annie.hn@gmail.com>
2025-07-11 22:27:47 +1000

    Add basic template into FormDemo.tsx

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
 
 export const FormDemo = () => {
-  return <div></div>;
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h4'>New Account</Typography>
+      </CardContent>
+    </Card>
+  );
 };
