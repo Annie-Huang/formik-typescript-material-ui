792fb14076aeaa35051e6f3d8a1f7965edb93572
Annie Huang <annie.hn@gmail.com>
2025-07-11 22:20:20 +1000

    Create boilerplate for <FormDemo>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FormDemo } from './FormDemo';
 
 function App() {
-  return <div></div>;
+  return <FormDemo />;
 }
 
 export default App;
---
+++ src/FormDemo.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FormDemo = () => {
+  return <div></div>;
+};
