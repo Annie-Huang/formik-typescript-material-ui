09d9af79b41c6fec65a010fdd029427878a2bba6
Annie Huang <annie.hn@gmail.com>
2025-07-12 15:25:58 +1000

    Put every field inside a <FormGroup> component

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Card,
   CardContent,
+  FormGroup,
   MenuItem,
   TextField,
   Typography,
@@ -27,75 +28,87 @@ export const FormDemo = () => {
         <Formik initialValues={initialValues} onSubmit={() => {}}>
           {({ values, errors, isSubmitting, isValidating }) => (
             <Form>
-              {/*<Field name='fullName' />*/}
-              <Field
-                name='fullName'
-                as={TextField}
-                label='Full Name'
-                variant='standard'
-              />
+              <FormGroup>
+                {/*<Field name='fullName' />*/}
+                <Field
+                  name='fullName'
+                  as={TextField}
+                  label='Full Name'
+                  variant='standard'
+                />
+              </FormGroup>
 
-              {/*<Field name='initialInvestment' type='number' />*/}
-              <Field
-                name='initialInvestment'
-                type='number'
-                as={TextField}
-                label='Initial Investment'
-                variant='standard'
-              />
+              <FormGroup>
+                {/*<Field name='initialInvestment' type='number' />*/}
+                <Field
+                  name='initialInvestment'
+                  type='number'
+                  as={TextField}
+                  label='Initial Investment'
+                  variant='standard'
+                />
+              </FormGroup>
 
-              {/*<Field name='investmentRisk' value='High' type='checkbox' />*/}
-              {/*<Field name='investmentRisk' value='Medium' type='checkbox' />*/}
-              {/*<Field name='investmentRisk' value='Low' type='checkbox' />*/}
-              <MyCheckbox
-                name='investmentRisk'
-                value='High'
-                label='High - Super Risky'
-              />
-              <MyCheckbox
-                name='investmentRisk'
-                value='Medium'
-                label='Medium - Risky'
-              />
-              <MyCheckbox
-                name='investmentRisk'
-                value='Low'
-                label='Low - Safe'
-              />
+              <FormGroup>
+                {/*<Field name='investmentRisk' value='High' type='checkbox' />*/}
+                {/*<Field name='investmentRisk' value='Medium' type='checkbox' />*/}
+                {/*<Field name='investmentRisk' value='Low' type='checkbox' />*/}
+                <MyCheckbox
+                  name='investmentRisk'
+                  value='High'
+                  label='High - Super Risky'
+                />
+                <MyCheckbox
+                  name='investmentRisk'
+                  value='Medium'
+                  label='Medium - Risky'
+                />
+                <MyCheckbox
+                  name='investmentRisk'
+                  value='Low'
+                  label='Low - Safe'
+                />
+              </FormGroup>
 
-              {/*<Field name='commentAboutInvestmentRisk' as='textarea' />*/}
-              <Field
-                name='commentAboutInvestmentRisk'
-                as={TextField}
-                multiline
-                rows={3}
-                rowsMax={10}
-                variant='standard'
-              />
+              <FormGroup>
+                {/*<Field name='commentAboutInvestmentRisk' as='textarea' />*/}
+                <Field
+                  name='commentAboutInvestmentRisk'
+                  as={TextField}
+                  multiline
+                  rows={3}
+                  rowsMax={10}
+                  variant='standard'
+                />
+              </FormGroup>
 
-              {/*<Field name='dependents' as='select'>*/}
-              {/*  <option value={0}>0</option>*/}
-              {/*  <option value={1}>1</option>*/}
-              {/*  <option value={2}>2</option>*/}
-              {/*  <option value={3}>3</option>*/}
-              {/*  <option value={4}>4</option>*/}
-              {/*  <option value={5}>5</option>*/}
-              {/*</Field>*/}
-              <Field name='dependents' as={TextField} select>
-                <MenuItem value={0}>0</MenuItem>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-              </Field>
+              <FormGroup>
+                {/*<Field name='dependents' as='select'>*/}
+                {/*  <option value={0}>0</option>*/}
+                {/*  <option value={1}>1</option>*/}
+                {/*  <option value={2}>2</option>*/}
+                {/*  <option value={3}>3</option>*/}
+                {/*  <option value={4}>4</option>*/}
+                {/*  <option value={5}>5</option>*/}
+                {/*</Field>*/}
+                <Field name='dependents' as={TextField} select>
+                  <MenuItem value={0}>0</MenuItem>
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                  <MenuItem value={4}>4</MenuItem>
+                  <MenuItem value={5}>5</MenuItem>
+                </Field>
+              </FormGroup>
 
-              {/* If you don't add value into the checkbox field like the investmentRisk field, it will just default to true|false value */}
-              {/*<Field name='acceptedTermsAndConditions' type='checkbox' />*/}
-              <MyCheckbox
-                name='acceptedTermsAndConditions'
-                label='Accept terms and conditions'
-              />
+              <FormGroup>
+                {/* If you don't add value into the checkbox field like the investmentRisk field, it will just default to true|false value */}
+                {/*<Field name='acceptedTermsAndConditions' type='checkbox' />*/}
+                <MyCheckbox
+                  name='acceptedTermsAndConditions'
+                  label='Accept terms and conditions'
+                />
+              </FormGroup>
 
               <pre>{JSON.stringify(values, null, 4)}</pre>
             </Form>
