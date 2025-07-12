754011db296fb7118aea2c17ec7a1cc8380dd989
Annie Huang <annie.hn@gmail.com>
2025-07-12 15:07:32 +1000

    Call useField in MyCheckbox component

--- src/MyCheckbox.tsx
+++ src/MyCheckbox.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import { Checkbox, FormControlLabel } from '@mui/material';
+import React, { FC } from 'react';
+import { Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
+import { useField } from 'formik';
 
-export const MyCheckbox = () => {
+export interface MyCheckboxProps extends CheckboxProps {
+  name: string;
+  value?: string | number;
+  label?: string;
+}
+
+export const MyCheckbox: FC<MyCheckboxProps> = (props) => {
+  const [field] = useField({
+    name: props.name,
+    type: 'checkbox',
+    value: props.value,
+  });
+
   return (
     <FormControlLabel control={<Checkbox defaultChecked />} label='Label' />
   );
