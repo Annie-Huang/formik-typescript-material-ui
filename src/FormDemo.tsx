8f48ab129efcc8a72f464e41d8752f130a6aa8ea
Annie Huang <annie.hn@gmail.com>
2025-07-12 13:00:21 +1000

    Create the boilert plate for <Formik> element

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import { Formik } from 'formik';
+
+const INITIAL_FORM_STATE = {};
 
 export const FormDemo = () => {
   return (
     <Card>
       <CardContent>
         <Typography variant='h4'>New Account</Typography>
+        <Formik initialValues={{ ...INITIAL_FORM_STATE }} onSubmit={() => {}}>
+          {({ values, errors, isSubmitting, isValidating }) => <div></div>}
+        </Formik>
       </CardContent>
     </Card>
   );
