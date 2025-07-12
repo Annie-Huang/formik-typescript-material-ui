ebc59e79bf4ce51b930bc9ba12321ca639568e32
Annie Huang <annie.hn@gmail.com>
2025-07-12 13:35:48 +1000

    Create textarea and select from <Field> in formik

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -27,6 +27,17 @@ export const FormDemo = () => {
               <Field name='investmentRisk' value='Medium' type='checkbox' />
               <Field name='investmentRisk' value='Low' type='checkbox' />
 
+              <Field name='commentAboutInvestmentRisk' as='textarea' />
+
+              <Field name='dependents' as='select'>
+                <option value={0}>0</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </Field>
+
               <pre>{JSON.stringify(values, null, 4)}</pre>
             </Form>
           )}
