54588df3df61d87e75ebdbc8241f99c90a973a67
Annie Huang <annie.hn@gmail.com>
2025-07-12 15:13:41 +1000

    Switch the termsAndCondition to use <MyCheckbox> as well

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -91,7 +91,11 @@ export const FormDemo = () => {
               </Field>
 
               {/* If you don't add value into the checkbox field like the investmentRisk field, it will just default to true|false value */}
-              <Field name='acceptedTermsAndConditions' type='checkbox' />
+              {/*<Field name='acceptedTermsAndConditions' type='checkbox' />*/}
+              <MyCheckbox
+                name='acceptedTermsAndConditions'
+                label='Accept terms and conditions'
+              />
 
               <pre>{JSON.stringify(values, null, 4)}</pre>
             </Form>
